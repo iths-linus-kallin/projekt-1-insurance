1fc533bb07956b089156c3aa90418c6eda73bb06
Linus Kallin <jaglinus@gmail.com>
2019-10-23 12:03:45 +0200

    Fler anim

--- js/main.js
+++ js/main.js
@@ -50,3 +50,23 @@ function slide(){
 }
 
 slide()
+
+function gradient(){
+var granimInstance = new Granim({
+    element: '#h1',
+    direction: 'left-right',
+    states : {
+        "default-state": {
+            gradients: [
+                ['#EB3349', '#F45C43'],
+                ['#FF8008', '#FFC837'],
+                ['#4CB8C4', '#3CD3AD'],
+                ['#24C6DC', '#514A9D'],
+                ['#FF512F', '#DD2476'],
+                ['#DA22FF', '#9733EE']
+            ],
+            transitionSpeed: 2000
+        }
+    }
+});
+}
